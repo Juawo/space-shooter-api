6c08d3389ce80d74e246096d5c3396c0abc7e1c9
Juawo <joaopedrojp2703@gmail.com>
2026-01-14 14:41:31 -0300

    fix: remove required modifier from Id property in Player model

--- Models/Player.cs
+++ Models/Player.cs
@@ -5,14 +5,18 @@ namespace SpaceShooterApi.Models;
 public class Player
 {
     [Key]
-    public required Guid Id { get;  set; }
+    public Guid Id { get;  set; }
     public required  string Nickname { get;  set; }
     public required int Age { get;  set; }
     public required string Country { get; set; }
 
+    public Player()
+    {
+        
+    }
+
     public Player(Guid id, string nickname, int age, string country)
     {
-        Id = id;
         Nickname = nickname;
         Age = age;
         Country = country;
